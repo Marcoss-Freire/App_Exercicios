6babcd08f119ac329c172928f13c9cee69e48b85
Marcoss-Freire <marcos.freire5@etec.sp.gov.br>
2023-08-13 16:12:00 -0300

    Finalizando parte lógica do exercício 6

--- app/src/main/java/com/example/app_exercicios/Exercicio_6.java
+++ app/src/main/java/com/example/app_exercicios/Exercicio_6.java
@@ -1,12 +1,14 @@
 package com.example.app_exercicios;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Exercicio_6 extends AppCompatActivity {
 
@@ -21,16 +23,34 @@ public class Exercicio_6 extends AppCompatActivity {
         cod_s = findViewById(R.id.rdb_cod_s);
         editxt_qtd = findViewById(R.id.editxt_qtd);
         btn_calcular = findViewById(R.id.btn_calcular);
-
+        txt = findViewById(R.id.txt_qtd);
 
         group.setOnCheckedChangeListener((group, checkedId) -> {
             RadioButton get_cod = findViewById(checkedId);
             cod = (String) get_cod.getText();
-            editxt_qtd.setText(cod);
         });
 
+        btn_calcular.setOnClickListener(v -> {
+            qtd = Integer.parseInt(editxt_qtd.getText().toString());
 
-
+            switch (cod) {
+                case "C":
+                    total = 2.0 * qtd;
+                    Toast resultado_C = Toast.makeText(this, "O valor a pagar é: R$ ".concat(String.valueOf(total)), Toast.LENGTH_LONG);
+                    resultado_C.show();
+                    break;
+                case "R":
+                    total = 2.5 * qtd;
+                    Toast resultado_R = Toast.makeText(this, "O valor a pagar é: R$ ".concat(String.valueOf(total)), Toast.LENGTH_LONG);
+                    resultado_R.show();
+                    break;
+                case "S":
+                    total = 1.5 * qtd;
+                    Toast resultado_S = Toast.makeText(this, "O valor a pagar é: R$ ".concat(String.valueOf(total)), Toast.LENGTH_LONG);
+                    resultado_S.show();
+                    break;
+            }
+        });
     }
 
     RadioGroup group;
@@ -38,6 +58,9 @@ public class Exercicio_6 extends AppCompatActivity {
     RadioButton cod_r;
     RadioButton cod_s;
     EditText editxt_qtd;
+    TextView txt;
     Button btn_calcular;
     String cod;
+    Integer qtd;
+    Double total;
 }

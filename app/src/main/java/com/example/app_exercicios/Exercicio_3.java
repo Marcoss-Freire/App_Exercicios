95cbfaf007102d507f47e80a22fd6a1fc7ed51e9
Marinaolisilva <marina.silva1077@etec.sp.gov.br>
2023-08-15 21:36:57 -0300

    Programando exercício 3

---
+++ app/src/main/java/com/example/app_exercicios/Exercicio_3.java
@@ -0,0 +1,114 @@
+package com.example.app_exercicios;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Exercicio_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercicio3);
+
+        // Instânciando os objetos
+
+       txt_dados = findViewById(R.id.txt_dados_exe3);
+       txt_home = findViewById(R.id.txt_home_exe3);
+       img_dados = findViewById(R.id.img_dados);
+       img_home = findViewById(R.id.exe3_icon_home);
+       edit_extras = findViewById(R.id.extras);
+       txt_extras = findViewById(R.id.horasE);
+       edit_faltas = findViewById(R.id.faltas);
+       txt_faltas = findViewById(R.id.horasF);
+       text_result = findViewById(R.id.txtresult);
+       btn_verificar = findViewById(R.id.verifica);
+
+       //programando o botão verificar
+        btn_verificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View horas) {
+                //criando variáveis para as EditTexts e convertendo os valores para double
+                double horasE = Double.parseDouble(edit_extras.getText().toString());
+                double horasF = Double.parseDouble(edit_faltas.getText().toString());
+
+                //Converter horas em minutos, para isso criando outra variável sendo elas: minutos extras e minutos-faltas
+                double minutosE = horasE * 60;
+                double minutosF = horasF * 60;
+
+
+                //Calculando as horas de acordo com a tabela
+                double H = minutosE - (2.0 / 3.0) * minutosF;
+
+                //Criando as condições de prêmios  da tabela, usando a estrutura if e else
+                if (H > 2400){
+                    text_result.setText("O prêmio é de R$500,00");
+                }
+                else if (H >= 1801 && H <= 2400){
+                    text_result.setText("O prêmio é de R$400,oo");
+                }
+                else if (H >= 1201 && H<=1800){
+                    text_result.setText("O prêmio é de R$300,oo");
+                }
+                else if (H >=600 && H<= 1200){
+                    text_result.setText("o Prêmio é de R$200,00");
+                }
+                else if (H <600){
+                    text_result.setText("O prêmio é de R$100,00");
+                }
+            }
+        });
+
+
+
+        // Programação NavBar
+
+        txt_dados.setOnClickListener(v -> {
+            Intent abrir_dados = new Intent(Exercicio_3.this, Dados.class);
+            startActivity(abrir_dados);
+        });
+
+        txt_home.setOnClickListener(v -> {
+            Intent abrir_home = new Intent(Exercicio_3.this, MainActivity.class);
+            startActivity(abrir_home);
+        });
+
+        img_home.setOnClickListener(v -> {
+            Intent abrir_home = new Intent(Exercicio_3.this, MainActivity.class);
+            startActivity(abrir_home);
+        });
+
+
+        img_dados.setOnClickListener(v -> {
+            Intent abrir_dados = new Intent(Exercicio_3.this, Dados.class);
+            startActivity(abrir_dados);
+        });
+    }
+
+    TextView txt_dados;
+    TextView txt_home;
+    ImageView img_dados;
+    ImageView img_home;
+    EditText edit_extras;
+    TextView txt_extras;
+    EditText edit_faltas;
+    TextView txt_faltas;
+    TextView text_result;
+    Button btn_verificar;
+
+
+
+
+
+
+
+
+
+}
--- app/src/main/java/com/example/app_exercicios/MainActivity.java
+++ app/src/main/java/com/example/app_exercicios/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.app_exercicios;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img_exe1 = findViewById(R.id.Home_img_exe1);
+        img_exe3 = findViewById(R.id.Home_img_exe3);
         img_exe6 = findViewById(R.id.Home_img_exe6);
         img_dados = findViewById(R.id.Home_icon_dados);
         txt_dados = findViewById(R.id.Home_txt_dados);
 
-        img_exe1.setOnClickListener(new View.OnClickListener(){
+        img_exe3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent abrir_exercicio = new Intent(MainActivity.this, Exercicio_1.class);
+                Intent abrir_exercicio = new Intent(MainActivity.this, Exercicio_3.class);
                 startActivity(abrir_exercicio);
             }
         });
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    ImageView img_exe1;
+    ImageView img_exe3;
     ImageView img_exe6;
     ImageView img_dados;
     TextView txt_dados;
